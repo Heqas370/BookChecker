cee5e21fd1e569ad7d30dd8ae5a45b6ca6973939
Adam <adamh07999@gmail.com>
2026-02-08 12:01:06 +0100

    Initial Commit

---
+++ BookChecker/BookCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookCheckerApp.swift
+//  BookChecker
+//
+//  Created by Adam Herman on 08/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct BookCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
